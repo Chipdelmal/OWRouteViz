cca4afb4278cd14ca7cf7cc827d732af5dc1dbe5
chipdelmal <chipdelmal@gmail.com>
2021-06-15 20:47:50 -0700

    Improving aesthetics of maps

--- random.py
+++ random.py
@@ -1,33 +1,40 @@
+
+import functions as fun
 import osmnx as ox
 from os import path
+import points as pts
 ox.config(log_console=True, use_cache=True)
 
 #  Source: https://github.com/CarlosLannister/beautifulMaps
+#   https://morioh.com/p/f30f0a215c2f
 
-
-point = (19.4928408, -99.2726895) # Mexico City
-(point, label, fName) = ((19.492925759295318, -99.27052605182259), 'CDMX', 'Mirtos')
-(point, label, fName) = ((37.8742145, -122.28609469999999), 'Berkeley, CA', 'Berkeley')
-
+(point, label, fName, bldg) = pts.BRK
 
 PATH = '/home/chipdelmal/Documents/OneWheel'
-bgcolor = "#000000"
-dist = 20000
+DPI = 500
+DST = 5000
+###############################################################################
+# Colors
+###############################################################################
+bgColor = "#000000"
+bdColor = '#ffffff22'
+rdColor = '#000b82'
+rdAlpha = .75
 ###############################################################################
 # Get Network
 ###############################################################################
 G = ox.graph_from_point(
-    point, dist=dist, network_type='all',
+    point, dist=DST, network_type='all',
     retain_all=True, simplify=True
 )
-gdf = ox.footprints.footprints_from_point(point=point, distance=dist)
+if bldg:
+    gdf = ox.geometries.geometries_from_point(
+        point, tags={'building': True} , dist=DST
+    )
 ###############################################################################
 # Process Roads
 ###############################################################################
-u = []
-v = []
-key = []
-data = []
+(u, v, key, data) = ([], [], [], [])
 for uu, vv, kkey, ddata in G.edges(keys=True, data=True):
     u.append(uu)
     v.append(vv)
@@ -39,22 +46,22 @@ roadWidths = []
 for item in data:
     if "length" in item.keys():
         if item["length"] <= 100:
-            linewidth = 0.10
-            color = "#5E7EBF"
+            linewidth = 0.10*6
+            color = fun.lighten(rdColor, .1)
         elif item["length"] > 100 and item["length"] <= 200:
-            linewidth = 0.15
-            color = "#5E7ECF"
+            linewidth = 0.15*6
+            color = fun.lighten(rdColor, .2)
         elif item["length"] > 200 and item["length"] <= 400:
-            linewidth = 0.25
-            color = "#5E7EDF"    
+            linewidth = 0.25*6  
+            color = fun.lighten(rdColor, .3)
         elif item["length"] > 400 and item["length"] <= 800:
-            color = "#5E7EEF"
-            linewidth = 0.35
+            linewidth = 0.35*6
+            color = fun.lighten(rdColor, .4)
         else:
-            color = "#5E7EFF"
-            linewidth = 0.45
+            linewidth = 0.45*6
+            color = fun.lighten(rdColor, .5)
     else:
-        color = "#5E7EFA"
+        color = rdColor
         linewidth = 0.10
     roadColors.append(color)
     roadWidths.append(linewidth)
@@ -63,19 +70,26 @@ for item in data:
 ###############################################################################
 (fig, ax) = ox.plot_graph(
     G, node_size=0,figsize=(27, 40), 
-    dpi = 300, bgcolor = bgcolor,
-    save = False, edge_color=roadColors,
-    edge_linewidth=roadWidths, edge_alpha=1, show=False
+    dpi=DPI, bgcolor=bgColor,
+    save=False, edge_color=roadColors, edge_alpha=rdAlpha,
+    edge_linewidth=roadWidths, show=False
 )
+if bldg:
+    (fig, ax) = ox.plot_footprints(
+        gdf,
+        color=bdColor,
+        ax=ax, dpi=DPI, save=False, 
+        show=False, close=False
+    )
 ax.scatter(
     point[1], point[0], marker="x",
-    c='#ff006e55', s=100, zorder=10
+    c='#ff006e00', s=100, zorder=10
 )
 ax.annotate(
     label, xy=(0, .5), xytext=(.5, .5), 
     xycoords='figure fraction', textcoords='figure fraction',
     horizontalalignment='center', verticalalignment='center',
-    color='#FFFFFFB5', fontsize=250
+    color='#ffffffCC', fontsize=250
 )
 ###############################################################################
 # Export
@@ -83,6 +97,6 @@ ax.annotate(
 fig.tight_layout(pad=0)
 fig.savefig(
     path.join(PATH, fName+'.png'), 
-    dpi=500, bbox_inches='tight', format="png", 
+    dpi=DPI, bbox_inches='tight', format="png", 
     facecolor=fig.get_facecolor(), transparent=False
 )
