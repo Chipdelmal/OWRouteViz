482708ec943be1a197a9b77ad0545f8db262cd60
chipdelmal <chipdelmal@gmail.com>
2021-06-17 08:29:19 -0700

    Added inkscape pipe

--- scrap.py
+++ scrap.py
@@ -69,3 +69,15 @@ tixPad = str(tix).zfill(4)
 fimgName = path.join(PATH, 'img', '{}-{}.png'.format(fName, 'final'))
 fig.savefig(fimgName, dpi=250, bbox_inches='tight', pad_inches=0, transparent=True)
 plt.close(fig)
+
+
+
+import matplotlib.font_manager
+from IPython.core.display import HTML
+
+def make_html(fontname):
+    return "<p>{font}: <span style='font-family:{font}; font-size: 24px;'>{font}</p>".format(font=fontname)
+
+code = "\n".join([make_html(font) for font in sorted(set([f.name for f in matplotlib.font_manager.fontManager.ttflist]))])
+
+HTML("<div style='column-count: 2;'>{}</div>".format(code))
